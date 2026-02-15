1ed125b67f2cd5766a69c75f9708d446a3d50b6f
Ahamed Nismi <mnanismi2000@gmail.com>
2026-02-15 15:35:51 +0530

    Problem 196: Delete Duplicate Emails

---
+++ Easy_Problems/196_delete_duplicate_emails.sql
@@ -0,0 +1,85 @@
+/*
+LeetCode Problem 196: Delete Duplicate Emails
+Difficulty: Easy
+
+Table: Person
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key (unique values).
+Each row contains an email (lowercase only).
+
+Task:
+Delete all duplicate emails, keeping only the row with the smallest id.
+After running the DELETE, the Person table should only have unique emails.
+
+Example:
+
+Input: 
+Person table:
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+
+Output:
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+*/
+
+DROP TABLE Person;  
+
+CREATE TABLE Person (
+    id INT PRIMARY KEY,
+    email VARCHAR(100)
+);
+
+INSERT INTO Person VALUES
+(1,'john@example.com'),
+(2,'bob@example.com'),
+(3,'john@example.com');
+
+
+-- Solution 1 
+
+WITH cte AS (
+    SELECT id,
+           ROW_NUMBER() OVER (
+               PARTITION BY email
+               ORDER BY id
+           ) AS rn
+    FROM Person
+)
+DELETE FROM Person
+WHERE id IN (
+    SELECT id
+    FROM cte
+    WHERE rn > 1
+);
+
+
+SELECT * FROM Person
+
+--Solution 2 Using Self Join 
+DELETE FROM Person
+WHERE id NOT IN (
+    SELECT MIN(id)
+    FROM Person
+    GROUP BY email
+);
+
+
+
+
+
+
