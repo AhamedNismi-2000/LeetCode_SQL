f00f433b3c23154f0ef3687b5662d086bd3a3025
Ahamed Nismi <mnanismi2000@gmail.com>
2026-02-19 11:54:40 +0530

    596. Classes With at Least 5 Students

---
+++ Easy_Problems/ss.sql
@@ -0,0 +1,75 @@
+/*  Table: Courses
+
+
+596. Classes With at Least 5 Students
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+(student, class) is the primary key (combination of columns with unique values) for this table.
+Each row of this table indicates the name of a student and the class in which they are enrolled.
+ 
+
+Write a solution to find all the classes that have at least five students.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Courses table:
++---------+----------+
+| student | class    |
++---------+----------+
+| A       | Math     |
+| B       | English  |
+| C       | Math     |
+| D       | Biology  |
+| E       | Math     |
+| F       | Computer |
+| G       | Math     |
+| H       | Math     |
+| I       | Math     |
++---------+----------+
+Output: 
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+
+
+  */
+
+
+DROP TABLE IF EXISTS Courses
+
+-- Create Courses table
+CREATE TABLE Courses (
+    student VARCHAR(50),
+    class   VARCHAR(50),
+    PRIMARY KEY (student, class)
+);
+
+-- Insert sample data into Courses
+INSERT INTO Courses (student, class) VALUES
+('A', 'Math'),
+('B', 'English'),
+('C', 'Math'),
+('D', 'Biology'),
+('E', 'Math'),
+('F', 'Computer'),
+('G', 'Math'),
+('H', 'Math'),
+('I', 'Math');
+
+
+
+SELECT * FROM courses
