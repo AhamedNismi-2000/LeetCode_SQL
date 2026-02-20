50a7c740a80df0c16ba7129fc60e2cbc76becf26
Ahamed Nismi <mnanismi2000@gmail.com>
2026-02-20 10:21:34 +0530

    619 Biggest Single Number

---
+++ Easy_Problems/619_biggest_single_number.sql
@@ -0,0 +1,99 @@
+/*
+
+619 Biggest Single Number 
+
+Table: MyNumbers
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| num         | int  |
++-------------+------+
+This table may contain duplicates (In other words, there is no primary key for this table in SQL).
+Each row of this table contains an integer.
+ 
+
+A single number is a number that appeared only once in the MyNumbers table.
+
+Find the largest single number. If there is no single number, report null.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+MyNumbers table:
++-----+
+| num |
++-----+
+| 8   |
+| 8   |
+| 3   |
+| 3   |
+| 1   |
+| 4   |
+| 5   |
+| 6   |
++-----+
+Output: 
++-----+
+| num |
++-----+
+| 6   |
++-----+
+Explanation: The single numbers are 1, 4, 5, and 6.
+Since 6 is the largest single number, we return it.
+Example 2:
+
+Input: 
+MyNumbers table:
++-----+
+| num |
++-----+
+| 8   |
+| 8   |
+| 7   |
+| 7   |
+| 3   |
+| 3   |
+| 3   |
++-----+
+Output: 
++------+
+| num  |
++------+
+| null |
++------+
+*/
+
+
+-- Drop The Table 
+DROP TABLE MyNumbers
+
+
+-- Create table
+CREATE TABLE MyNumbers (
+    num INT
+);
+
+-- Insert values (Example 1)
+INSERT INTO MyNumbers (num) VALUES
+(8),
+(8),
+(3),
+(3),
+(1),
+(4),
+(5),
+(6);
+
+-- Insert values (Example 2)
+INSERT INTO MyNumbers (num) VALUES
+(8),
+(8),
+(7),
+(7),
+(3),
+(3),
+(3);
