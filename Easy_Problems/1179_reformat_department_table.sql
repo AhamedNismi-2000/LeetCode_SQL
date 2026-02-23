0d025cff2d2b143fe64de7fdd02c5cb719e2fa07
Ahamed Nismi <mnanismi2000@gmail.com>
2026-02-23 13:27:52 +0530

    1179. Reformat Department #### Solution

--- Easy_Problems/1179_reformat_department_table.sql
+++ Easy_Problems/1179_reformat_department_table.sql
@@ -67,7 +67,7 @@ INSERT INTO Department (id, revenue, month) VALUES
 SELECT * FROM department
 
 
--- Soulution 
+-- Soulution 1
 
 SELECT
     id,
@@ -85,3 +85,36 @@ SELECT
     MAX(CASE WHEN month = 'Dec' THEN revenue END) AS Dec_Revenue
 FROM Department
 GROUP BY id;
+
+
+
+
+-- Solution 2
+
+SELECT DISTINCT
+    d.id,
+    j.revenue AS Jan_Revenue,
+    f.revenue AS Feb_Revenue,
+    m.revenue AS Mar_Revenue,
+    a.revenue AS Apr_Revenue,
+    ma.revenue AS May_Revenue,
+    jn.revenue AS Jun_Revenue,
+    jl.revenue AS Jul_Revenue,
+    au.revenue AS Aug_Revenue,
+    s.revenue AS Sep_Revenue,
+    o.revenue AS Oct_Revenue,
+    n.revenue AS Nov_Revenue,
+    d2.revenue AS Dec_Revenue
+FROM Department d
+LEFT JOIN Department j  ON d.id = j.id AND j.month = 'Jan'
+LEFT JOIN Department f  ON d.id = f.id AND f.month = 'Feb'
+LEFT JOIN Department m  ON d.id = m.id AND m.month = 'Mar'
+LEFT JOIN Department a  ON d.id = a.id AND a.month = 'Apr'
+LEFT JOIN Department ma ON d.id = ma.id AND ma.month = 'May'
+LEFT JOIN Department jn ON d.id = jn.id AND jn.month = 'Jun'
+LEFT JOIN Department jl ON d.id = jl.id AND jl.month = 'Jul'
+LEFT JOIN Department au ON d.id = au.id AND au.month = 'Aug'
+LEFT JOIN Department s  ON d.id = s.id AND s.month = 'Sep'
+LEFT JOIN Department o  ON d.id = o.id AND o.month = 'Oct'
+LEFT JOIN Department n  ON d.id = n.id AND n.month = 'Nov'
+LEFT JOIN Department d2 ON d.id = d2.id AND d2.month = 'Dec';
