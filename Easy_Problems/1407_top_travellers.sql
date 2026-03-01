3c9dd4a6b37ed7de07149a775faeeaf60c5fee7b
Ahamed Nismi <mnanismi2000@gmail.com>
2026-03-01 20:25:28 +0530

    1407. Top Travellers Solution 2  ####

--- Easy_Problems/1407_top_travellers.sql
+++ Easy_Problems/1407_top_travellers.sql
@@ -136,3 +136,17 @@ LEFT JOIN rides r
     ON u.id = r.user_id  -- Leetcode Throw Error without the "id"
 GROUP BY u.name
 ORDER BY travelled_distance DESC, u.name;
+
+
+-- Solution 2 
+SELECT 
+    u.name,
+    CASE 
+        WHEN SUM(r.distance) IS NULL THEN 0
+        ELSE SUM(r.distance)
+    END AS travelled_distance
+FROM users u
+LEFT JOIN rides r
+    ON u.id = r.user_id
+GROUP BY u.id, u.name
+ORDER BY travelled_distance DESC, u.name;
