0694a1e08d149407338e9b4e037ac724bdc85a92
Ahamed Nismi <mnanismi2000@gmail.com>
2026-03-01 16:28:10 +0530

    1280 Students and Examinations #### Solution 1

--- Easy_Problems/1280_students_and_examinations.sql
+++ Easy_Problems/1280_students_and_examinations.sql
@@ -150,3 +150,26 @@ INSERT INTO Examinations (student_id, subject_name) VALUES
 (13, 'Physics'),
 (2, 'Math'),
 (1, 'Math');
+
+
+-- Solution 1
+    SELECT
+        s.student_id,
+        s.student_name,
+        sub.subject_name,
+        COUNT(e.subject_name) AS attended_exams
+    FROM Students s
+    CROSS JOIN Subjects sub
+    LEFT JOIN Examinations e
+        ON e.student_id = s.student_id
+    AND e.subject_name = sub.subject_name
+    GROUP BY
+        s.student_id,
+        s.student_name,
+        sub.subject_name
+    ORDER BY
+        s.student_id,
+        sub.subject_name;
+
+
+        
