5f152c5b15b2bf1fcbfb1f6fe3bb391b5dbfdda1
Ahamed Nismi <mnanismi2000@gmail.com>
2026-03-01 17:11:10 +0530

    1327. List the Products Ordered in a Period #### Solution 2

--- Easy_Problems/1327_list_the_products_ordered_in_a_period.sql
+++ Easy_Problems/1327_list_the_products_ordered_in_a_period.sql
@@ -135,3 +135,17 @@ SELECT
     total_units AS unit
 FROM sold_feb
 WHERE total_units >= 100;
+
+
+
+-- Solution 2
+    SELECT 
+        p.product_name,
+        SUM(o.unit) AS total_units
+    FROM Products p
+    JOIN Orders o
+      ON p.product_id = o.product_id
+    WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
+    GROUP BY p.product_name
+    HAVING SUM(o.unit) >=100
+
