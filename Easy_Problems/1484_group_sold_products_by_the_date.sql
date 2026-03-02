515f45527670d429d02939a275c80e235f26d738
Ahamed Nismi <mnanismi2000@gmail.com>
2026-03-02 12:22:36 +0530

    1484. Group Sold Products By The Date  #### Solution

--- Easy_Problems/1484_group_sold_products_by_the_date.sql
+++ Easy_Problems/1484_group_sold_products_by_the_date.sql
@@ -71,3 +71,11 @@ INSERT INTO Activities (sell_date, product) VALUES
 
 
 SELECT * FROM activities
+
+-- Solution 
+
+SELECT sell_date,
+       COUNT(DISTINCT product) AS num_sold,
+      STRING_AGG(DISTINCT product, ',' ORDER BY product) AS products
+FROM  activities
+GROUP BY sell_date 
