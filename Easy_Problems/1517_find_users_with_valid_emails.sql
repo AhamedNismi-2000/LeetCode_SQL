82053f291a29af14517983c4ebae2c70443eb519
Ahamed Nismi <mnanismi2000@gmail.com>
2026-03-02 12:44:11 +0530

    1517. Find Users With Valid E-Mails  #### Solution

--- Easy_Problems/1517_find_users_with_valid_emails.sql
+++ Easy_Problems/1517_find_users_with_valid_emails.sql
@@ -77,3 +77,9 @@ INSERT INTO Users (user_id, name, mail) VALUES
 
 
 SELECT * FROM users;
+
+SELECT user_id,
+       name,
+       mail
+FROM Users
+WHERE mail ~ '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\.com$';
