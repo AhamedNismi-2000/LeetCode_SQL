1104a37d47c24467f53b03b4fa8663c685524a0e
Ahamed Nismi <mnanismi2000@gmail.com>
2026-03-06 19:01:46 +0530

    1527. Patients With a Condition ### Solution

--- Easy_Problems/number_1000/1527_patients_with_a_condition.sql
+++ Easy_Problems/number_1000/1527_patients_with_a_condition.sql
@@ -60,3 +60,15 @@ INSERT INTO Patients (patient_id, patient_name, conditions) VALUES
 (3, 'Bob', 'DIAB100 MYOP'),
 (4, 'George', 'ACNE DIAB100'),
 (5, 'Alain', 'DIAB201');
+
+
+
+-- Solution 
+
+SELECT 
+    patient_id,
+    patient_name,
+    conditions
+FROM Patients
+WHERE conditions LIKE 'DIAB1%'
+   OR conditions LIKE '% DIAB1%';
