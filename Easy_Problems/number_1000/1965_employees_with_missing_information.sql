ab80e104c9f1e4c10a93260d612c16e5aa025297
Ahamed Nismi <mnanismi2000@gmail.com>
2026-03-09 11:34:01 +0530

    1965 Employees With Missing Information ###Solution 1

---
+++ Easy_Problems/number_1000/1965_employees_with_missing_information.sql
@@ -0,0 +1,106 @@
+/*
+
+1965. Employees With Missing Information
+
+
+Table: Employees
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
++-------------+---------+
+employee_id is the column with unique values for this table.
+Each row of this table indicates the name of the employee whose ID is employee_id.
+ 
+
+Table: Salaries
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| salary      | int     |
++-------------+---------+
+employee_id is the column with unique values for this table.
+Each row of this table indicates the salary of the employee whose ID is employee_id.
+ 
+
+Write a solution to report the IDs of all the employees with missing information. The information of an employee is missing if:
+
+The employee's name is missing, or
+The employee's salary is missing.
+Return the result table ordered by employee_id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employees table:
++-------------+----------+
+| employee_id | name     |
++-------------+----------+
+| 2           | Crew     |
+| 4           | Haven    |
+| 5           | Kristian |
++-------------+----------+
+Salaries table:
++-------------+--------+
+| employee_id | salary |
++-------------+--------+
+| 5           | 76071  |
+| 1           | 22517  |
+| 4           | 63539  |
++-------------+--------+
+Output: 
++-------------+
+| employee_id |
++-------------+
+| 1           |
+| 2           |
++-------------+
+Explanation: 
+Employees 1, 2, 4, and 5 are working at this company.
+The name of employee 1 is missing.
+The salary of employee 2 is missing.
+
+
+*/
+
+
+
+CREATE TABLE Employees (
+    employee_id INT PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+CREATE TABLE Salaries (
+    employee_id INT PRIMARY KEY,
+    salary INT
+);
+
+INSERT INTO Employees (employee_id, name) VALUES
+(2, 'Crew'),
+(4, 'Haven'),
+(5, 'Kristian');
+
+INSERT INTO Salaries (employee_id, salary) VALUES
+(5, 76071),
+(1, 22517),
+(4, 63539);
+
+
+  -- Solution 1
+
+    SELECT COALESCE(s.employee_id,
+            e.employee_id) AS employee_id
+    FROM employees e 
+    FULL JOIN salaries s 
+    ON e.employee_id = s.employee_id
+    WHERE e.employee_id IS NULL OR s.salary IS NULL
+
+
+   
