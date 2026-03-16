42dfd018c19bdd336abf1ec5ab7aa30148336df5
Ahamed Nismi <mnanismi2000@gmail.com>
2026-03-16 14:40:35 +0530

    585_Investments in 2016 ###Solution 2 CTE II

--- Medium_Problems/number_100/585_investments_in_2016.sql
+++ Medium_Problems/number_100/585_investments_in_2016.sql
@@ -1,114 +1,153 @@
 /*
-585. Investments in 2016
-
-
-Table: Insurance
-
-+-------------+-------+
-| Column Name | Type  |
-+-------------+-------+
-| pid         | int   |
-| tiv_2015    | float |
-| tiv_2016    | float |
-| lat         | float |
-| lon         | float |
-+-------------+-------+
-pid is the primary key (column with unique values) for this table.
-Each row of this table contains information about one policy where:
-pid is the policyholder's policy ID.
-tiv_2015 is the total investment value in 2015 and tiv_2016 is the total investment value in 2016.
-lat is the latitude of the policy holder's city. It's guaranteed that lat is not NULL.
-lon is the longitude of the policy holder's city. It's guaranteed that lon is not NULL.
-
-
-Write a solution to report the sum of all total investment values in 2016 tiv_2016, for all policyholders who:
-
-have the same tiv_2015 value as one or more other policyholders, and
-are not located in the same city as any other policyholder (i.e., the (lat, lon) attribute pairs must be unique).
-Round tiv_2016 to two decimal places.
-
-The result format is in the following example.
-
-
-
-Example 1:
-
-Input: 
-Insurance table:
-+-----+----------+----------+-----+-----+
-| pid | tiv_2015 | tiv_2016 | lat | lon |
-+-----+----------+----------+-----+-----+
-| 1   | 10       | 5        | 10  | 10  |
-| 2   | 20       | 20       | 20  | 20  |
-| 3   | 10       | 30       | 20  | 20  |
-| 4   | 10       | 40       | 40  | 40  |
-+-----+----------+----------+-----+-----+
-Output: 
-+----------+
-| tiv_2016 |
-+----------+
-| 45.00    |
-+----------+
-Explanation: 
-The first record in the table, like the last record, meets both of the two criteria.
-The tiv_2015 value 10 is the same as the third and fourth records, and its location is unique.
-
-The second record does not meet any of the two criteria. Its tiv_2015 is not like any other policyholders and its location is the same as the third record, which makes the third record fail, too.
-So, the result is the sum of tiv_2016 of the first and last record, which is 45.
-
-
+ 585. Investments in 2016
+ 
+ 
+ Table: Insurance
+ 
+ +-------------+-------+
+ | Column Name | Type  |
+ +-------------+-------+
+ | pid         | int   |
+ | tiv_2015    | float |
+ | tiv_2016    | float |
+ | lat         | float |
+ | lon         | float |
+ +-------------+-------+
+ pid is the primary key (column with unique values) for this table.
+ Each row of this table contains information about one policy where:
+ pid is the policyholder's policy ID.
+ tiv_2015 is the total investment value in 2015 and tiv_2016 is the total investment value in 2016.
+ lat is the latitude of the policy holder's city. It's guaranteed that lat is not NULL.
+ lon is the longitude of the policy holder's city. It's guaranteed that lon is not NULL.
+ 
+ 
+ Write a solution to report the sum of all total investment values in 2016 tiv_2016, for all policyholders who:
+ 
+ have the same tiv_2015 value as one or more other policyholders, and
+ are not located in the same city as any other policyholder (i.e., the (lat, lon) attribute pairs must be unique).
+ Round tiv_2016 to two decimal places.
+ 
+ The result format is in the following example.
+ 
+ 
+ 
+ Example 1:
+ 
+ Input: 
+ Insurance table:
+ +-----+----------+----------+-----+-----+
+ | pid | tiv_2015 | tiv_2016 | lat | lon |
+ +-----+----------+----------+-----+-----+
+ | 1   | 10       | 5        | 10  | 10  |
+ | 2   | 20       | 20       | 20  | 20  |
+ | 3   | 10       | 30       | 20  | 20  |
+ | 4   | 10       | 40       | 40  | 40  |
+ +-----+----------+----------+-----+-----+
+ Output: 
+ +----------+
+ | tiv_2016 |
+ +----------+
+ | 45.00    |
+ +----------+
+ Explanation: 
+ The first record in the table, like the last record, meets both of the two criteria.
+ The tiv_2015 value 10 is the same as the third and fourth records, and its location is unique.
+ 
+ The second record does not meet any of the two criteria. Its tiv_2015 is not like any other policyholders and its location is the same as the third record, which makes the third record fail, too.
+ So, the result is the sum of tiv_2016 of the first and last record, which is 45.
+ 
+ 
  */
-
-CREATE TABLE
-    Insurance (
-        pid INT PRIMARY KEY,
-        tiv_2015 FLOAT,
-        tiv_2016 FLOAT,
-        lat FLOAT NOT NULL,
-        lon FLOAT NOT NULL
-    );
-
-INSERT INTO
-    Insurance (pid, tiv_2015, tiv_2016, lat, lon)
-VALUES
-    (1, 10, 5, 10, 10),
+CREATE TABLE Insurance (
+    pid INT PRIMARY KEY,
+    tiv_2015 FLOAT,
+    tiv_2016 FLOAT,
+    lat FLOAT NOT NULL,
+    lon FLOAT NOT NULL
+);
+INSERT INTO Insurance (pid, tiv_2015, tiv_2016, lat, lon)
+VALUES (1, 10, 5, 10, 10),
     (2, 20, 20, 20, 20),
     (3, 10, 30, 20, 20),
     (4, 10, 40, 40, 40);
 
-    -- Solution 1 SubQuery 
 
-    SELECT ROUND(SUM(tiv_2016)::decimal,2)
-    FROM insurance
-    WHERE tiv_2015 IN  (
-        SELECT   
-            tiv_2015
+-- Solution 1 SubQuery 
+
+SELECT ROUND(SUM(tiv_2016)::decimal, 2)
+FROM insurance
+WHERE tiv_2015 IN (
+        SELECT tiv_2015
         FROM insurance
-        GROUP BY tiv_2015    
+        GROUP BY tiv_2015
         HAVING COUNT(*) > 1
-    ) AND 
-    (lon,lat) IN (
-      SELECT lat, lon
-      FROM Insurance
-      GROUP BY lat, lon
-      HAVING COUNT(*) = 1
     )
+    AND (lon, lat) IN (
+        SELECT lat,
+            lon
+        FROM Insurance
+        GROUP BY lat,
+            lon
+        HAVING COUNT(*) = 1
 
- -- Solution 2 CTE 
 
+
+        
+    ) -- Solution 2 CTE 
     WITH same_tiv AS (
-      SELECT tiv_2015
-      FROM insurance
-      GROUP BY tiv_2015
-      HAVING COUNT(*) > 1 
-    ) ,
-    same_location AS (
-        SELECT lon,lat
+        SELECT tiv_2015
         FROM insurance
-        GROUP BY lon,lat
+        GROUP BY tiv_2015
+        HAVING COUNT(*) > 1
+    ),
+    same_location AS (
+        SELECT lon,
+            lat
+        FROM insurance
+        GROUP BY lon,
+            lat
         HAVING COUNT(*) = 1
     )
-    SELECT ROUND(SUM(tiv_2016)::decimal,2)
-    FROM insurance 
-    WHERE tiv_2015 IN (SELECT tiv_2015 FROM same_tiv)
-    AND  (lat,lon) IN (SELECT lon,lat FROM same_location)
+SELECT ROUND(SUM(tiv_2016)::decimal, 2)
+FROM insurance
+WHERE tiv_2015 IN (
+        SELECT tiv_2015
+        FROM same_tiv
+    )
+    AND (lat, lon) IN (
+        SELECT lon,
+            lat
+        FROM same_location
+    )
+    
+    
+    
+    
+    -- Solution 2 CTE II  
+
+    WITH same_tiv AS (
+        SELECT pid
+        FROM insurance
+        WHERE tiv_2015 IN (
+                SELECT tiv_2015
+                FROM insurance
+                GROUP BY tiv_2015
+                HAVING COUNT(*) > 1
+            )
+    ),
+    same_location AS (
+        SELECT pid
+        FROM insurance
+        WHERE (lat, lon) IN (
+                SELECT lat,
+                    lon
+                FROM Insurance
+                GROUP BY lat,
+                    lon
+                HAVING COUNT(*) = 1
+            )
+    )
+SELECT ROUND(SUM(tiv_2016)::decimal, 2)
+FROM insurance AS i
+    JOIN same_tiv AS st ON st.pid = i.pid
+    JOIN same_location AS sl ON sl.pid = i.pid
