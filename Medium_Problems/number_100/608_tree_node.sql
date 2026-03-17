590ee676e6db6a538bf7e7b60e409bdfc774b7c7
Ahamed Nismi <mnanismi2000@gmail.com>
2026-03-17 16:40:12 +0530

    608. Tree Node ### Solution 1

--- Medium_Problems/number_100/608_tree_node.sql
+++ Medium_Problems/number_100/608_tree_node.sql
@@ -88,3 +88,18 @@ INSERT INTO Tree (id, p_id) VALUES
 (3, 1),
 (4, 2),
 (5, 2);
+
+
+
+ -- Solution 1
+   
+  SELECT id,
+       CASE 
+           WHEN p_id IS NULL THEN 'Root'
+           WHEN id NOT IN (
+               SELECT p_id 
+               FROM Tree 
+               WHERE p_id IS NOT NULL
+           ) THEN 'Leaf'
+           ELSE 'Inner' END AS type 
+       FROM Tree;
