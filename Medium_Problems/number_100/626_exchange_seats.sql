0d820c71c6d5c94bf7572a6f1a2d11fa295299bc
Ahamed Nismi <mnanismi2000@gmail.com>
2026-03-22 11:06:38 +0530

    626 Exchange Seats ###Solution

--- Medium_Problems/number_100/626_exchange_seats.sql
+++ Medium_Problems/number_100/626_exchange_seats.sql
@@ -64,3 +64,17 @@ INSERT INTO Seat (id, student) VALUES
 (3, 'Emerson'),
 (4, 'Green'),
 (5, 'Jeames');
+
+
+--Solution
+
+
+SELECT 
+    CASE 
+        WHEN id%2=1 AND id != (SELECT MAX(id) FROM Seat) THEN id+1
+        WHEN id%2=0 THEN id-1
+        ELSE id
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
