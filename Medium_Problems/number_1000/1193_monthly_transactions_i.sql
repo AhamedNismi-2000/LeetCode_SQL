5ac8859f40d26f22cd6fc8b62a7aaab51c6a52e9
Ahamed Nismi <mnanismi2000@gmail.com>
2026-03-23 11:57:11 +0530

    1193 Monthly Transactions I ### Solution 1

--- Medium_Problems/number_1000/1193_monthly_transactions_i.sql
+++ Medium_Problems/number_1000/1193_monthly_transactions_i.sql
@@ -1,5 +1,5 @@
 /*
-1193. Monthly Transactions I
+1193 Monthly Transactions I
 
 
 Table: Transactions
@@ -64,3 +64,15 @@ INSERT INTO Transactions (id, country, state, amount, trans_date) VALUES
 (122, 'US', 'declined', 2000, '2018-12-19'),
 (123, 'US', 'approved', 2000, '2019-01-01'),
 (124, 'DE', 'approved', 2000, '2019-01-07');
+
+
+-- Solution 1 
+SELECT 
+    TO_CHAR(trans_date , 'YYYY-MM') AS month,
+    country,
+    COUNT(*) AS trans_count,
+    COUNT(CASE WHEN state='approved' THEN 1 END ) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state='approved' THEN amount ELSE 0 END ) AS approved_total_amount
+FROM transactions 
+GROUP BY  TO_CHAR(trans_date , 'YYYY-MM'), country;
