1cf8946d4c19845509b4f59fde47aa69f02c56a3
Ahamed Nismi <mnanismi2000@gmail.com>
2026-04-02 13:09:44 +0530

    1907 Count Salary Categories  ### Solution 2

--- Medium_Problems/number_1000/1907_count_salary_categories.sql
+++ Medium_Problems/number_1000/1907_count_salary_categories.sql
@@ -92,6 +92,34 @@ WITH categories AS (
     LEFT JOIN account_category ac
     ON ac.category = c.category
     GROUP BY c.category
+
+
+
+    
+  -- ### Solution 2
+
+WITH categories AS (
+    SELECT 'Low Salary'     AS category
+    UNION ALL
+    SELECT 'Average Salary' AS category
+    UNION ALL
+    SELECT 'High Salary'    AS category
+)
+SELECT 
+    c.category,
+    COUNT(a.account_id) AS accounts_count
+FROM categories c
+LEFT JOIN (
+    SELECT 
+        account_id,
+        CASE 
+            WHEN income < 20000                THEN 'Low Salary'
+            WHEN income BETWEEN 20000 AND 50000 THEN 'Average Salary'
+            WHEN income > 50000                THEN 'High Salary'
+        END AS category
+    FROM Accounts 
+) a ON c.category = a.category
+GROUP BY c.category;    
         
 
 
