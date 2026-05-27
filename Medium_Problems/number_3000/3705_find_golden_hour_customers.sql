cb83ae821593ab2c3f24121aa18afef2d8cdb719
Ahamed Nismi <mnanismi2000@gmail.com>
2026-05-27 14:52:37 +0530

    3705 Find Golden Hour Customers ### Solution Leetcode

--- Medium_Problems/number_3000/3705_find_golden_hour_customers.sql
+++ Medium_Problems/number_3000/3705_find_golden_hour_customers.sql
@@ -194,4 +194,32 @@ INSERT INTO restaurant_orders (
 
         ORDER BY ar.average_rating  DESC , ar.customer_id DESC           
 
-   
+
+   --- ### Solution  Leetcode Version
+
+ WITH customer_stats AS (
+    SELECT
+        customer_id,
+        COUNT(order_id)  AS total_orders,
+        SUM(CASE WHEN order_rating IS NOT NULL THEN 1 ELSE 0 END)  AS rated_orders,
+        ROUND(AVG(order_rating), 2)  AS average_rating,
+        SUM(CASE 
+                WHEN EXTRACT(HOUR FROM order_timestamp) BETWEEN 11 AND 14
+                  OR EXTRACT(HOUR FROM order_timestamp) BETWEEN 18 AND 21
+                THEN 1 ELSE 0 
+            END)  AS peak_orders
+    FROM restaurant_orders
+    GROUP BY customer_id
+)
+    SELECT
+        customer_id,
+        total_orders,
+        ROUND(peak_orders * 100.0 / total_orders, 2) AS peak_hour_percentage,
+        average_rating
+    FROM customer_stats
+    WHERE total_orders   >= 3
+    AND average_rating >= 4.0
+    AND ROUND(peak_orders  * 100.0 / total_orders, 2) >= 60
+    AND rated_orders * 100.0 / total_orders >= 50
+    ORDER BY average_rating DESC, customer_id DESC;
+        
