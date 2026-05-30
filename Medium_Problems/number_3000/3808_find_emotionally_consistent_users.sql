fa687291502b4e9c57c21fedb8d3f45164e0a5dd
Ahamed Nismi <mnanismi2000@gmail.com>
2026-05-30 13:33:03 +0530

    3808 Find Emotionally Consistent Users ### Solution 2

--- Medium_Problems/number_3000/3808_find_emotionally_consistent_users.sql
+++ Medium_Problems/number_3000/3808_find_emotionally_consistent_users.sql
@@ -141,3 +141,28 @@ VALUES
         AND total_reactions >= 5
         AND reaction_count * 1.0 / total_reactions >= 0.60
     ORDER BY reaction_ratio DESC, user_id ASC;
+
+
+
+
+    --- ### Solution 2 
+
+      WITH reaction_stats AS (
+    SELECT
+        user_id,
+        reaction,
+        COUNT(*) AS reaction_count,
+        SUM(COUNT(*)) OVER (PARTITION BY user_id) AS total_reactions,
+        RANK() OVER (PARTITION BY user_id ORDER BY COUNT(*) DESC ) AS rn
+    FROM reactions
+    GROUP BY user_id, reaction
+)
+    SELECT
+        user_id,
+        reaction AS dominant_reaction,
+        ROUND(reaction_count * 1.0 / total_reactions, 2) AS reaction_ratio
+    FROM reaction_stats
+    WHERE rn = 1
+    AND total_reactions >= 5
+    AND reaction_count * 1.0 / total_reactions >= 0.6
+    ORDER BY reaction_ratio DESC, user_id ASC;
