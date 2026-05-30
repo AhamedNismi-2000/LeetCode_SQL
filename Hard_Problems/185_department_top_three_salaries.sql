ef6c3cd4e96f1dc1bf50c53a69fbf8069a75a5a7
Ahamed Nismi <mnanismi2000@gmail.com>
2026-05-30 13:54:11 +0530

    185 Department Top Three Salaries

--- Hard_Problems/185_department_top_three_salaries.sql
+++ Hard_Problems/185_department_top_three_salaries.sql
@@ -89,28 +89,47 @@ There are no employees with the exact same name, salary and department.  give ta
 
 */
 
-CREATE TABLE Department (
-    id INT PRIMARY KEY,
-    name VARCHAR(255)
-);
+    CREATE TABLE Department (
+        id INT PRIMARY KEY,
+        name VARCHAR(255)
+    );
 
-CREATE TABLE Employee (
-    id INT PRIMARY KEY,
-    name VARCHAR(255),
-    salary INT,
-    departmentId INT,
-    FOREIGN KEY (departmentId) REFERENCES Department(id)
-);
+    CREATE TABLE Employee (
+        id INT PRIMARY KEY,
+        name VARCHAR(255),
+        salary INT,
+        departmentId INT,
+        FOREIGN KEY (departmentId) REFERENCES Department(id)
+    );
 
-INSERT INTO Department (id, name) VALUES
-(1, 'IT'),
-(2, 'Sales');
+    INSERT INTO Department (id, name) VALUES
+    (1, 'IT'),
+    (2, 'Sales');
 
-INSERT INTO Employee (id, name, salary, departmentId) VALUES
-(1, 'Joe', 85000, 1),
-(2, 'Henry', 80000, 2),
-(3, 'Sam', 60000, 2),
-(4, 'Max', 90000, 1),
-(5, 'Janet', 69000, 1),
-(6, 'Randy', 85000, 1),
-(7, 'Will', 70000, 1);
+    INSERT INTO Employee (id, name, salary, departmentId) VALUES
+    (1, 'Joe', 85000, 1),
+    (2, 'Henry', 80000, 2),
+    (3, 'Sam', 60000, 2),
+    (4, 'Max', 90000, 1),
+    (5, 'Janet', 69000, 1),
+    (6, 'Randy', 85000, 1),
+    (7, 'Will', 70000, 1);
+
+
+    --- ### Solution 1 
+     
+
+-- A company's executives are interested in seeing who earns the most money in each of the company's departments. 
+-- A high earner in a department is an employee who has a salary in the top three unique salaries for that department.
+
+-- Write a solution to find the employees who are high earners in each of the departments.
+
+-- Return the result table in any order.
+
+   SELECT * FROM department;
+   SELECT * FROM employee
+
+   SELECT 
+
+    d.name AS department,
+       
