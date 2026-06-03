5edf65d94be749082ec824511c69134145528325
Ahamed Nismi <mnanismi2000@gmail.com>
2026-06-03 13:32:36 +0530

    1517 Find Users With Valid E-Mails ### Solution Leetcode

--- Easy_Problems/number_1000/1517_find_users_with_valid_emails.sql
+++ Easy_Problems/number_1000/1517_find_users_with_valid_emails.sql
@@ -1,6 +1,6 @@
 /*
 
-1517. Find Users With Valid E-Mails
+1517 Find Users With Valid E-Mails
 
 
 Table: Users
@@ -75,11 +75,19 @@ INSERT INTO Users (user_id, name, mail) VALUES
 (6, 'David', 'david69@gmail.com'),
 (7, 'Shapiro', '.shapo@leetcode.com');
 
+  --- ### Solution 1
 
 SELECT * FROM users;
 
-SELECT user_id,
-       name,
-       mail
-FROM Users
-WHERE mail ~ '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\.com$';
+    SELECT user_id,
+        name,
+        mail
+    FROM Users
+    WHERE mail ~ '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\.com$';
+
+
+    --- ### Solution Leetcode 
+
+     SELECT user_id, name, mail
+     FROM Users
+     WHERE REGEXP_LIKE(mail, '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode[.]com$', 'c')
