891c90c238ef3af99d46fe5361760310da529adc
Ahamed Nismi <mnanismi2000@gmail.com>
2026-09-24 10:57:13 +0530

    86. Customer Placing the Largest Number of Orders Solution 4

--- Easy_Problems/number_100/586_customer_placing_the_largest_number_of_orders.sql
+++ Easy_Problems/number_100/586_customer_placing_the_largest_number_of_orders.sql
@@ -1,99 +1,114 @@
-/*
-586. Customer Placing the Largest Number of Orders
+    /*
+    586. Customer Placing the Largest Number of Orders
 
 
-Table: Orders
-+-----------------+----------+
-| Column Name     | Type     |
-+-----------------+----------+
-| order_number    | int      |
-| customer_number | int      |
-+-----------------+----------+
+    Table: Orders
+    +-----------------+----------+
+    | Column Name     | Type     |
+    +-----------------+----------+
+    | order_number    | int      |
+    | customer_number | int      |
+    +-----------------+----------+
 
-order_number is the primary key (column with unique values) for this table.
-This table contains information about the order ID and the customer ID.
- 
+    order_number is the primary key (column with unique values) for this table.
+    This table contains information about the order ID and the customer ID.
+    
 
-Write a solution to find the customer_number for the customer who has placed the largest number of orders.
+    Write a solution to find the customer_number for the customer who has placed the largest number of orders.
 
-The test cases are generated so that exactly one customer will have placed more orders than any other customer.
+    The test cases are generated so that exactly one customer will have placed more orders than any other customer.
 
-The result format is in the following example.
+    The result format is in the following example.
 
- 
+    
 
-Example 1:
+    Example 1:
 
-Input: 
-Orders table:
-+--------------+-----------------+
-| order_number | customer_number |
-+--------------+-----------------+
-| 1            | 1               |
-| 2            | 2               |
-| 3            | 3               |
-| 4            | 3               |
-+--------------+-----------------+
-Output: 
+    Input: 
+    Orders table:
+    +--------------+-----------------+
+    | order_number | customer_number |
+    +--------------+-----------------+
+    | 1            | 1               |
+    | 2            | 2               |
+    | 3            | 3               |
+    | 4            | 3               |
+    +--------------+-----------------+
+    Output: 
 
-+-----------------+
-| customer_number |
-+-----------------+
-| 3               |
-+-----------------+
+    +-----------------+
+    | customer_number |
+    +-----------------+
+    | 3               |
+    +-----------------+
 
-*/
+    */
 
-
--- File: 586_customer_placing_the_largest_number_of_orders.sql
-
-
-DROP TABLE IF EXISTS Orders;
-
--- Create Orders table
-CREATE TABLE Orders (
-    order_number INT PRIMARY KEY,
-    customer_number INT
-);
-
--- Insert sample data into Orders
-INSERT INTO Orders (order_number, customer_number) VALUES
-(1, 1),
-(2, 2),
-(3, 3),
-(4, 3);
+    -- File: 586_customer_placing_the_largest_number_of_orders.sql
 
 
--- Solution 1
+    DROP TABLE IF EXISTS Orders CASCADE ;
 
-SELECT customer_number
-FROM Orders
-GROUP BY customer_number
-ORDER BY COUNT(*) DESC
-LIMIT 1
+    -- Create Orders table
+    CREATE TABLE Orders (
+        order_number INT PRIMARY KEY,
+        customer_number INT
+    );
 
--- Solution 2 Using CTE 
+    -- Insert sample data into Orders
+    INSERT INTO Orders (order_number, customer_number) VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 3);
 
-WITH high_order AS(
-    SELECT customer_number,
-    COUNT(*) OVER(PARTITION BY customer_number) AS order_count 
+
+    ### Solution 1
+
+    SELECT customer_number
     FROM Orders
-)
-SELECT customer_number
-FROM high_order
-ORDER BY  order_count DESC
-LIMIT 1 ;
+    GROUP BY customer_number
+    ORDER BY COUNT(*) DESC
+    LIMIT 1
 
--- Solution 2 Using Subquery
+    ### Solution 2 Using CTE 
 
-SELECT customer_number
-FROM (
-    SELECT customer_number,
-           COUNT(*) OVER (PARTITION BY customer_number) AS order_count
-    FROM Orders
-) t
-ORDER BY order_count DESC
-LIMIT 1;
+    WITH high_order AS(
+        SELECT customer_number,
+        COUNT(*) OVER(PARTITION BY customer_number) AS order_count 
+        FROM Orders
+    )
+    SELECT customer_number
+    FROM high_order
+    ORDER BY  order_count DESC
+    LIMIT 1 ;
+
+    -- Solution 2 Using Subquery
+
+    SELECT customer_number
+    FROM (
+        SELECT customer_number,
+            COUNT(*) OVER (PARTITION BY customer_number) AS order_count
+        FROM Orders
+    ) t
+    ORDER BY order_count DESC
+    LIMIT 1;
 
 
+    ### Solution 4
 
+   SELECT 
+       customer_number 
+  FROM (     
+    SELECT 
+        customer_number,
+        RANK() OVER( ORDER BY order_count DESC ) AS rnk 
+   FROM  (
+        SELECT 
+            COUNT(order_number) AS order_count,
+            customer_number
+        FROM orders
+        GROUP BY customer_number 
+    )   
+  )
+  WHERE rnk = 1 
